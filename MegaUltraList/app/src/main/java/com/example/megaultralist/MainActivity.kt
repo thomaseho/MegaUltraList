31a51425a12f92475154b1ad28a45f59471fd1ac
Thomas Egebakken Honnemyr <thomaseho@uia.no>
2021-03-24 12:43:39 +0100

    Added firebase support. A method to save the users to do lists will come later.

--- MegaUltraList/app/src/main/java/com/example/megaultralist/MainActivity.kt
+++ MegaUltraList/app/src/main/java/com/example/megaultralist/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.megaultralist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.megaultralist.databinding.ActivityMainBinding
 import com.example.megaultralist.tasks.data.Task
 import com.example.megaultralist.tasks.data.toDoList
 import com.example.megaultralist.tasks.toDoListCollectionAdapter
 import com.example.megaultralist.tasks.toDoListDetailsActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 const val EXTRA_TODOLIST_INFO: String = "com.example.megaultralist.tasks.info"
-const val REQUEST_BOOK_DETAILS:Int = 564567
+const val REQUEST_TODOLIST_DETAILS:Int = 564567
 
 class ToDoListHolder {
 
@@ -24,6 +28,9 @@ class ToDoListHolder {
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+    private val TAG:String = "Mega Ultra List:Mainactivity"
 
     private var listCollection:MutableList<toDoList> = mutableListOf(
             toDoList(listName = "Handleliste", tasks = mutableListOf(Task("Brød", false),
@@ -63,10 +70,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        auth = Firebase.auth
+        signInAnonymously()
+
         setContentView(binding.root)
 
         binding.toDoListListing.layoutManager = LinearLayoutManager(this)
         binding.toDoListListing.adapter = toDoListCollectionAdapter(listCollection, this::onToDoListClicked)
+
+    }
+
+    private fun signInAnonymously(){
+
+        auth.signInAnonymously().addOnSuccessListener {
+            Log.d(TAG, "Login success ${it.user}")
+        }.addOnFailureListener {
+            Log.e(TAG, "Login failed", it)
+        }
+
     }
 
     private fun addToDoList(listName: String, tasks: MutableList<Task>){
@@ -85,7 +107,7 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == REQUEST_BOOK_DETAILS){
+        if (requestCode == REQUEST_TODOLIST_DETAILS){
 
         }
     }
